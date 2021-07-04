035b6f4accc2cf2d94f2c76e533a6ba464b9e5cd
AliTinai <ali-009@windowslive.com>
2021-07-04 10:56:02 +0300

    Number of grid squares can now be chosen

--- scripts/etchsketch.js
+++ scripts/etchsketch.js
@@ -1,9 +1,36 @@
 
-let container = document.querySelector('#container');
+const container = document.querySelector('#container');
 
-let divArray = [];
+function clearGrid(){
 
-for(let i = 0; i < 256; i++){
-  divArray[i] = document.createElement('div');
-  container.appendChild(divArray[i]);
+  const currentGrid = document.querySelectorAll('#container > div');
+
+  if(currentGrid){
+    currentGrid.forEach((item) => container.removeChild(item));
+  }
 }
+
+
+function adjustGrid(e){
+
+  clearGrid();
+
+  //create the grid based on squareNumber
+  let squareNumber = prompt('Enter the number of squares for the Sketchpad: ');
+
+  container.style.gridTemplateColumns = `repeat(${+squareNumber},${400/+squareNumber}px)`;
+
+  container.style.gridTemplateRows = `repeat(${+squareNumber},${400/+squareNumber}px)`;
+
+  //creating the divs inside the container
+  let divArray = [];
+  for(let i = 0; i < squareNumber**2; i++){
+    divArray[i] = document.createElement('div');
+    container.appendChild(divArray[i]);
+  }
+
+
+}
+
+const sizeButton = document.querySelector('#sizeButton');
+sizeButton.addEventListener('click', adjustGrid);
