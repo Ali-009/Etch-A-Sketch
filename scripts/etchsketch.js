aafdc3398c9c13fb995bc289bb3cb3dc2fcf28f3
AliTinai <ali-009@windowslive.com>
2021-07-04 11:27:09 +0300

    Hovering the mouse now leaves a trial

--- scripts/etchsketch.js
+++ scripts/etchsketch.js
@@ -16,7 +16,12 @@ function adjustGrid(e){
   clearGrid();
 
   //create the grid based on squareNumber
-  let squareNumber = prompt('Enter the number of squares for the Sketchpad: ');
+  let squareNumber = prompt('Enter the number of squares (between 0 and 100) for the Sketchpad: ');
+
+  if(+squareNumber > 100){
+    alert('Please input a number below 100');
+    return;
+  }
 
   container.style.gridTemplateColumns = `repeat(${+squareNumber},${400/+squareNumber}px)`;
 
@@ -26,6 +31,10 @@ function adjustGrid(e){
   let divArray = [];
   for(let i = 0; i < squareNumber**2; i++){
     divArray[i] = document.createElement('div');
+
+    //making sure the divs leave a trial like a pen
+    divArray[i].addEventListener('mouseover',
+      (e) => event.target.classList.add('trial'));
     container.appendChild(divArray[i]);
   }
 
