f2cf005697ec555a6b09d3251ac5b9724483262a
AliTinai <ali-009@windowslive.com>
2021-07-04 07:55:22 +0300

    Created the starting 16x16 grid

---
+++ scripts/etchsketch.js
@@ -0,0 +1,10 @@
+
+let container = document.querySelector('#container');
+
+let divArray = [];
+
+for(let i = 0; i < 256; i++){
+  divArray[i] = document.createElement('div');
+  container.appendChild(divArray[i]);
+  divArray[i].textContent = `Div ${i+1}`;
+}
